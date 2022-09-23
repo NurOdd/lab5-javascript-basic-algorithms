bb85546283476dcd30f87bd3b333dc9be7c9c10a
Nuria Rodriguez <nur.rodga@gmail.com>
2022-09-23 18:12:59 +0200

    update loops
    3.1 y 3.2

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ console.log("I'm ready")
 
 // Iteration 1: Names and Input
 //
-const hacker1 = 'Teodorico'; 
+const hacker1 = 'John'; 
 console.log(`The drive's name is ${hacker1}`);
 
 const hacker2 = 'Marcus';
@@ -19,8 +19,17 @@ if (hacker1.length<hacker2.length) {
     console.log(`Wow, you both have equally long names, ${hacker1.length}  characters!`)
 }
 
-
-
-
-
 // Iteration 3: Loops
+/*3.1 Print all the characters of the driver's name, separated by a space and in capitals i.e. "J O H N"
+
+3.2 Print all the characters of the navigator's name, in reverse order. i.e. "nhoJ"
+
+3.3 Depending on the lexicographic order of the strings, print:
+- The driver's name goes first.
+- Yo, the navigator goes first definitely.
+- What?! You both have the same name?*/
+
+console.log(hacker1.toUpperCase().split(''))
+
+console.log(hacker2.split('').reverse().join(''))
+
